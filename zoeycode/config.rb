54a5faa6db5f3fe756794aa87496ddf338ce5d2f
Zoey Heckman <43184084+zoeyheckman@users.noreply.github.com>
2019-11-20 19:16:36 -0600

    11/20

---
+++ zoeycode/config.rb
@@ -0,0 +1,24 @@
+require 'compass/import-once/activate'
+# Require any additional compass plugins here.
+
+http_path = "C:/Users/zeope/Desktop/zoeycode/html"
+css_dir = ""
+sass_dir = ""
+images_dir = "images"
+generated_images_path = "C:/Users/zeope/Desktop/zoeycode/generatedimages"
+javascripts_dir = "javascripts"
+fonts_dir = "fonts"
+
+output_style = :nested
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+line_comments = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass  scss && rm -rf sass && mv scss sass
